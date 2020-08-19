b08f60204b2608245a99141bec1a2d950f1bdfb5
LE VINH THANH <levinhthanh.dt1@gmail.com>
2020-08-19 10:07:02 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -200,5 +200,5 @@
 
 
 @section('javascript')
-    <script src="/libs/node_modules/jquery/dist/jquery.js"></script>
-    <script src="/js/welcome_page.js"></script>
+    <script src="{{ asset('libs/node_modules/jquery/dist/jquery.js') }}"></script>
+    <script src="{{ asset('js/welcome_page.js') }}"></script>
